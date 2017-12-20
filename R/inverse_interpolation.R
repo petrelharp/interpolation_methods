6bf6f4ff275bbaf905d4df4b4e4ec1ecfb53081a
peter <petrel.harp@gmail.com>
2017-12-20 14:36:17 -0800

    now omega can be a vector

--- R/inverse_interpolation.R
+++ R/inverse_interpolation.R
@@ -3,12 +3,14 @@
 #' @param x The matrix of "training" values of x, with one row per training set.
 #' @param y The matrix of "training" values of y corresponding to x.
 #' @param ypred The matrix of "test" values of y for which a value of x will be predicted.
-#' @param omega The bandwidth on the local weights.
+#' @param omega The bandwidth on the local weights (can be a vector of length equal to the number of columns of y).
 #' @export
 #' @return The matrix of predicted x values, with dimensions (nrow(ypred), ncol(x)).
 inverse_interpolation <- function (x, y, ypred, omega) {
+    y <- sweep(y, 2, omega, "/")
+    ypred <- sweep(y, 2, omega, "/")
     dvec <- fields::rdist(y, ypred)
-    w <- exp(-dvec/omega^2)
+    w <- exp(-dvec^2)
     w <- sweep(w, 2, colSums(w), "/")
     xpred <- crossprod(w, x)
     return(xpred)
