a101820fd8ca434a8cdfd18a4f3070028750e1fa
Jaime Ashander <jashander@ucdavis.edu>
2017-12-21 11:13:41 -0800

    fixen

--- R/inverse_interpolation.R
+++ R/inverse_interpolation.R
@@ -1,6 +1,6 @@
 #' Local inverse interpolation
 #'
-#' @param x The matrix of "training" values of x, with one item per training set.
+#' @param x The matrix of "training" values of x, with one row per training set.
 #' @param y The matrix of "training" values of y corresponding to x.
 #' @param ypred The matrix of "test" values of y for which a value of x will be predicted.
 #' @param omega The bandwidth on the local weights (can be a vector of length equal to the number of columns of y).
@@ -27,35 +27,37 @@ inverse_interpolation <- function (x, y, ypred, omega) {
 #' @param x The matrix of "training" values of x, with one row per training set.
 #' @param y The matrix of "training" values of y corresponding to x.
 #' @param ypred The matrix of "test" values of y for which a value of x will be predicted.
-#' @param seed The random seed used in the k-fold cross-validation.
-#' @param min_omega The minimum bandwidth attempted. 
-#' @param max_omega The initial maximum bandwidth attempted. 
+#' @param min_omega The minimum bandwidth attempted.
+#' @param max_omega The maximum bandwidth attempted. 
 #' @param k The number of folds to use in cross-validation.
+#' @param offset 
+#' @details The best omega is that in (min_omega, max_omega) which minimizes the
+#'          median relative error among the k cross-validation tests.
 #' @export
 #' @return List of `omega_min`, the omega that minimizes the cross-validation error,
 #'                 `ypred` the matrix of predicted x values for the optimal
 #'                 omega, with dimensions (nrow(ypred), ncol(x)), and
 #'                 `boundary_value` a boolean indicating if `omega_min` is on
 #'                 the boundary of the range of omegas examined.
-automatic_inverse_interpolation <- function (x, y, ypred, seed, min_omega, max_omega=NA, k=5) {
-    max_omega <- 50 * min_omega
+automatic_inverse_interpolation <- function (x, y, ypred, min_omega, k=5,
+					     max_omega=50*min_omega, offset=0) {
+    if (any(x <= 0) && any(x >= 0) && offset == 0) {
+	    warning("Values of x span zero: should set nonzero offset.")
+    }
     omegas <- seq(min_omega, max_omega, length.out=100)
-    idxs <- 1:length(x)
-    set.seed(seed)
-    shuffled_idxs <- sample(idxs, length(idxs), replace=FALSE)
-    folds <- split(shuffled_idxs, cut(shuffled_idxs, breaks=k))
-    best_omegas <- sapply(1:length(folds), function(i) {
-        test <- folds[[i]]
-        train <- unname(do.call(c, folds[-i]))
-    	errors <- sapply(omegas, function(omega) {
-    	    xpred <- inverse_interpolation(x=x[train], y=y[train, ], y[test, ], omega=omega)
-    	    rel_err <- abs(xpred - x[test]) / x[test]
-    	    sum(rel_err)
-            })
-    	best_omega <- omegas[which(min(errors) == errors)]
-    	best_omega
+    shuffled_idxs <- sample.int(nrow(x))
+    folds <- split(shuffled_idxs, cut(seq_along(shuffled_idxs), breaks=k)) 
+    median_rel_error <- sapply(omegas, function(omega) {
+        rel_error <- sapply(1:length(folds), function(i) {
+           test <- folds[[i]]
+           train <- unname(do.call(c, folds[-i]))
+    	   xpred <- inverse_interpolation(x=x[train, ], y=y[train, ], y[test, ], omega=omega)
+    	   rel_err <- abs(xpred - x[test, ]) / (offset + abs(x[test, ]))
+    	   sum(rel_err)
         })
-    best_omega <- median(do.call(c, as.list(best_omegas)))
+	median(rel_error)
+        })
+    best_omega <- omegas[which.min(median_rel_error)]
     return(list(omega_min=best_omega,
 		ypred=inverse_interpolation(x=x, y=y, ypred=ypred, omega=best_omega),
 	        boundary_value=! (min(omegas) < best_omega && best_omega < max(omegas))
